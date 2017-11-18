053504045ed7fd3ab71de1e95956d7460b8ad2e1
Rafael Freitas <rafael.vasconcelos@outlook.com>
2017-11-17 23:12:35 -0300

    Nova Activity sendo chamada.

--- app/src/main/java/com/example/rafael/catraca_web_app/NewClient.java
+++ app/src/main/java/com/example/rafael/catraca_web_app/NewClient.java
@@ -1,35 +1,30 @@
 package com.example.rafael.catraca_web_app;
 
+/**
+ * Created by rafael on 17/11/17.
+ */
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class NewClient extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_client);
 
-        final TextView txtView = this.findViewById(R.id.newClient);
+        final TextView txtView = this.findViewById(R.id.isClient);
         txtView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,NewClient.class);
+                Intent intent = new Intent(NewClient.this,MainActivity.class);
                 startActivity(intent);
             }
         });
-
-
-
-
-
     }
-
-
-
-
 }
