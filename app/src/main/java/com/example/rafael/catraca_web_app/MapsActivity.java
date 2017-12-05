3307683dbf51fb1d2bd4f982d4426b89e7d6068b
matheus.silva <matheus.silva@mv.com.br>
2017-12-05 11:38:29 -0300

    Alteração no design da landscape"about" e melhoria no mapa, agora finalmente listando o lugar correto.

--- app/src/main/java/com/example/rafael/catraca_web_app/MapsActivity.java
+++ app/src/main/java/com/example/rafael/catraca_web_app/MapsActivity.java
@@ -1,8 +1,16 @@
 package com.example.rafael.catraca_web_app;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,11 +20,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import basic.Usuario;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
     double latitude;
     double longitude;
+    private static final int MY_LOCATION_REQUEST_CODE = 1;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +36,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        if (checkLocationPermission()) {
+            callConnection();
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        mMap = googleMap;
-
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
@@ -41,14 +54,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setIndoorLevelPickerEnabled(true);
         mMap.setBuildingsEnabled(true);
         mMap.setIndoorEnabled(true);
+    }
 
+    public void exibirNoMapa(double latitude, double longitude){
+        if ((latitude < 0 && longitude < 0) && (mMap != null)) {
+            LatLng marcacao = new LatLng(latitude, longitude);
+            MarkerOptions mo = new MarkerOptions();
+            mo.position(marcacao).title("Sua localização atual");
+            mMap.addMarker(mo);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marcacao, 15));
+        }
+    }
 
-        LatLng marcacao = new LatLng(latitude, longitude);
-        MarkerOptions mo = new MarkerOptions();
-        mo.position(marcacao).title("Sua localização atual");
-        mMap.addMarker(mo);
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    public boolean checkLocationPermission()
+    {
+        String permission = "android.permission.ACCESS_FINE_LOCATION";
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private synchronized void callConnection(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("LOG", "onConnected" + bundle + ")");
+
+        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (l != null) {
+            Log.i("LOG", "Latitude:" + l.getLatitude() + ")");
+            Log.i("LOG", "longitude" + l.getLongitude() + ")");
+            longitude = l.getLongitude();
+            latitude = l.getLatitude();
+            exibirNoMapa(latitude, longitude);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(getApplication(), "Conexão foi suspensa!", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(getApplication(), "Conexão não foi bem sucedida!", Toast.LENGTH_LONG);
     }
 }
