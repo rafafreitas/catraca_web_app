99d2ee824888f504a456559916fb01c0d08973d5
Rafael Freitas <rafael.vasconcelos@outlook.com>
2017-12-05 22:11:02 -0300

    * Alterações na Tela para Gerar Visitas

--- app/src/main/java/com/example/rafael/catraca_web_app/NewVisitActivity.java
+++ app/src/main/java/com/example/rafael/catraca_web_app/NewVisitActivity.java
@@ -5,15 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.view.animation.Animation;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 import basic.Auth;
+import basic.Responsaveis;
+import basic.VisitaMotivo;
 import util.Internet;
+import util.Util;
 
 public class NewVisitActivity extends AppCompatActivity {
     private TextWatcher cpfMask;
 
     private Vibrator vib;
     Animation animShake;
+
+    private Button btnCadastro;
 
     private Internet internet;
     private Auth auth; //SingleUser
@@ -22,5 +30,28 @@ public class NewVisitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_visit);
+
+        auth = Auth.getInstance();
+        internet = new Internet(this);
+        Util.setCtxAtual(this);
+
+        initSpinner(auth.getResponsaveis(), auth.getVisitaMotivos());
+
     }
+
+
+    private void initSpinner(ArrayList<Responsaveis> responsaveis, ArrayList<VisitaMotivo> visitaMotivos){
+
+        for (Responsaveis responsavel : responsaveis){
+            //Será implementado o código para alimentar o spiner
+        }
+        for (VisitaMotivo visitaMotivo : visitaMotivos){
+            //Será implementado o código para alime//Será implementado o código para aliemntar o spinerntar o spiner
+        }
+
+
+    }
+
+
+
 }
