754a0dc53f4b33ef7ec9eff1fc8bf9b5b1b8cea7
Rafael Freitas <rafael.vasconcelos@outlook.com>
2017-12-06 21:48:37 -0300

    * Correção de erros na geração da Activity de Visitas. * Implementação da Classe Requester de Veículos.

--- app/src/main/java/fragments/FragmentHome.java
+++ app/src/main/java/fragments/FragmentHome.java
@@ -1,11 +1,14 @@
 package fragments;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.rafael.catraca_web_app.NewVisitsActivity;
 import com.example.rafael.catraca_web_app.R;
 
 /**
@@ -28,6 +31,24 @@ public class FragmentHome extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CardView novaVisita = (CardView) view.findViewById(R.id.cv_gerar_visita);
+        novaVisita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),NewVisitsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        CardView visitasAtivas = (CardView) view.findViewById(R.id.cv_visitas_ativas);
+        visitasAtivas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do what you want on click event
+            }
+        });
 
 //        ((TextView) view.findViewById(R.id.fragmentText)).setText("One");
 
--- app/src/main/java/request/RequesterVeiculo.java
+++ app/src/main/java/request/RequesterVeiculo.java
@@ -1,9 +1,18 @@
 package request;
 
+
 import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 import basic.Auth;
+import basic.Usuario;
 import basic.Veiculos;
+import util.Util;
 
 /**
  * Created by rafael on 06/12/17.
@@ -13,21 +22,107 @@ public class RequesterVeiculo {
 
     private Auth auth;
     private Context context;
-    private Veiculos veiculos;
 
-    public RequesterVeiculo() {
+    public RequesterVeiculo(){
+
     }
 
-    public Context getContext() {
-        return context;
+    public Context getContext(){
+        return this.context;
     }
 
-    public void setContext(Context context) {
+    public void setContext(Context context){
         this.context = context;
     }
 
-    public Veiculos checkVeiculoByPlaca(String placaSearch){
+    public void saveVeiculo(Veiculos veiculo) throws JSONException, InterruptedException, ExecutionException{
 
-        return veiculos;
+        final JSONObject json = new JSONObject();
+        auth = Auth.getInstance();
+
+        json.put("veic_id", veiculo.getVeic_id());
+        json.put("veic_placa", veiculo.getVeic_placa());
+        json.put("veic_modelo", veiculo.getVeic_modelo());
+        json.put("veic_foto", veiculo.getVeic_foto());
+
+        BaseRequester baseRequester = new BaseRequester();
+        baseRequester.setUrl(Requester.API_URL + "/veiculo/cadastro");
+        baseRequester.setAuthorization(auth.getToken());
+        baseRequester.setMethod(Method.POST);
+        baseRequester.setJsonString(json.toString());
+        baseRequester.setContext(context);
+
+        String jsonResponse = baseRequester.execute(baseRequester).get();
+        Log.d("API", jsonResponse);
+
+        JSONObject jsonObjectResponse = new JSONObject(jsonResponse);
+        auth.setStatusAPI(jsonObjectResponse.get("status").toString());
+        auth.setMessage(jsonObjectResponse.get("message").toString());
+
+        if(jsonObjectResponse.get("message").toString().equals("ERROR")){
+            //Informar o usuário
+            String errorMessage = jsonObjectResponse.get("result").toString();
+            auth.setMensagemErroApi(errorMessage);
+            Log.d("API",errorMessage);
+        }else{
+            //Atualizar o objeto de informações principais
+            AddAuth a = new AddAuth();
+            a.feedAuth(jsonObjectResponse);
+        }
     }
+
+    public Veiculos checkVeiculoByPlaca(String placa)throws JSONException, InterruptedException, ExecutionException{
+
+        final JSONObject json = new JSONObject();
+        auth = Auth.getInstance();
+        Usuario user = auth.getUsuario();
+
+        json.put("user_nome", user.getUser_nome());
+        json.put("user_email", user.getUser_email());
+        json.put("user_senha", user.getUser_senha());
+        json.put("user_cpf", user.getUser_cpf());
+        json.put("user_data_nasc", user.getUser_data_nasc());
+        json.put("veic_placa", placa);
+
+        BaseRequester baseRequester = new BaseRequester();
+        baseRequester.setUrl(Requester.API_URL + "/veiculo/consulta");
+        baseRequester.setAuthorization(auth.getToken());
+        baseRequester.setMethod(Method.POST);
+        baseRequester.setJsonString(json.toString());
+        baseRequester.setContext(context);
+
+        String jsonResponse = baseRequester.execute(baseRequester).get();
+        Log.d("API", jsonResponse);
+
+        JSONObject jsonObjectResponse = new JSONObject(jsonResponse);
+        auth.setStatusAPI(jsonObjectResponse.get("status").toString());
+        auth.setMessage(jsonObjectResponse.get("message").toString());
+
+        if(jsonObjectResponse.get("message").toString().equals("ERROR")){
+            //Informar o usuário
+            String errorMessage = jsonObjectResponse.get("result").toString();
+            auth.setMensagemErroApi(errorMessage);
+            Log.d("API",errorMessage);
+            return null;
+        }else{
+            //Atualizar o objeto de informações principais
+            AddAuth a = new AddAuth();
+            a.feedAuth(jsonObjectResponse.getJSONObject("gerais"));
+            JSONObject vj = jsonObjectResponse.getJSONObject("veiculo");
+            return  getFromJson(vj);
+        }
+    }
+
+    private Veiculos getFromJson(JSONObject objectVeiculo) throws JSONException{
+
+        Veiculos v = new Veiculos();
+        v.setVeic_id(Integer.parseInt(objectVeiculo.get("veic_id").toString()));
+        v.setVeic_placa(Util.formatVeiculoPlaca(objectVeiculo.get("veic_placa").toString()));
+        v.setVeic_modelo(objectVeiculo.get("veic_modelo").toString());
+        v.setVeic_foto(objectVeiculo.get("veic_foto").toString());
+        v.setUser_id(Integer.parseInt(objectVeiculo.get("user_id").toString()));
+
+        return v;
+    }
+
 }
